8afe91eb007f9b5455caf9fe96daa8a6221f1482
Gautam Salhotra <gautams3@users.noreply.github.com>
2024-02-09 15:16:43 -0800

    PCB example: tune controller, update target pose

--- serl_robot_infra/franka_env/envs/pcb_env/config.py
+++ serl_robot_infra/franka_env/envs/pcb_env/config.py
@@ -1,6 +1,6 @@
 import numpy as np
 from franka_env.envs.franka_env import DefaultEnvConfig
-
+from franka_env.utils.rotations import quat_2_euler
 
 class PCBEnvConfig(DefaultEnvConfig):
     """Set the configuration for FrankaEnv."""
@@ -12,14 +12,22 @@ class PCBEnvConfig(DefaultEnvConfig):
     }
     TARGET_POSE = np.array(
         [
-            0.5868045273591798,
-            0.012909963287586323,
-            0.06566930637555402,
-            3.1405188,
-            0.0,  # -0.041635,
-            0.0,  # -0.0306968,
+            0.6378155575883534,
+            -0.06302125722373966,
+            0.073,
+            3.12308208,
+            -0.01842338,
+            0.06769943,
         ]
     )
+    # currpose = requests.post(url + "getpos").json()['pose']
+    # sample currpose = [0.6378155575883534,-0.06302125722373966,0.07161134991397702,0.9993448317889017,0.033755161543103485,0.009519101764174228,0.008937726087444324]
+    # sample currpose = [0.6370623577389048,-0.06343594171797502,0.07222356478376063,0.9998436663527301,0.0035192305543307423,-0.016541550334071255,0.005160909142885387]
+    # sample currpose = [0.6362208204983791,-0.06280020351570716,0.07253667840286478,0.9998103195884877,0.011944632536439424,-0.008667952911610422,0.012708941350786162]
+    # target_posn = currpose[:3]
+    # target_quat = currpose[3:]
+    # target_euler = quat_2_euler(target_quat)
+    # TARGET_POSE = np.concatenate([target_posn, target_euler])
     RESET_POSE = TARGET_POSE + np.array([0.0, 0.0, 0.04, 0.0, 0.0, 0.0])
     REWARD_THRESHOLD = [0.003, 0.003, 0.001, 0.1, 0.1, 0.1]
     ACTION_SCALE = (0.02, 0.2, 1)
@@ -49,29 +57,30 @@ class PCBEnvConfig(DefaultEnvConfig):
     COMPLIANCE_PARAM = {
         "translational_stiffness": 3000,
         "translational_damping": 180,
+        "translational_Ki": 0,
         "rotational_stiffness": 150,
         "rotational_damping": 7,
-        "translational_Ki": 0,
+        "rotational_Ki": 0,
         "translational_clip_x": 0.0025,
         "translational_clip_y": 0.0015,
         "translational_clip_z": 0.002,
-        "translational_clip_neg_x": 0.002,
+        "translational_clip_neg_x": 0.003,
         "translational_clip_neg_y": 0.002,
         "translational_clip_neg_z": 0.003,
         "rotational_clip_x": 0.025,
-        "rotational_clip_y": 0.007,
+        "rotational_clip_y": 0.01,
         "rotational_clip_z": 0.01,
         "rotational_clip_neg_x": 0.025,
-        "rotational_clip_neg_y": 0.007,
+        "rotational_clip_neg_y": 0.01,
         "rotational_clip_neg_z": 0.01,
-        "rotational_Ki": 0,
     }
     PRECISION_PARAM = {
         "translational_stiffness": 3000,
         "translational_damping": 89,
+        "translational_Ki": 0.1,
         "rotational_stiffness": 300,
         "rotational_damping": 9,
-        "translational_Ki": 0.1,
+        "rotational_Ki": 0.1,
         "translational_clip_x": 0.01,
         "translational_clip_y": 0.01,
         "translational_clip_z": 0.01,
@@ -84,5 +93,4 @@ class PCBEnvConfig(DefaultEnvConfig):
         "rotational_clip_neg_x": 0.05,
         "rotational_clip_neg_y": 0.05,
         "rotational_clip_neg_z": 0.05,
-        "rotational_Ki": 0.1,
     }
