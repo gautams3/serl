25313572ee6b2746d20253c50fa1162bed9e85b5
Gautam Salhotra <gautams3@users.noreply.github.com>
2024-02-12 15:52:51 -0800

    improve PCB component pick script

--- examples/move_transformer_above_PCB.py
+++ examples/move_transformer_above_PCB.py
@@ -1,5 +1,6 @@
 """ Scipt to move the transformer above the PCB. """
 
+import os
 import requests
 import time
 import numpy as np
@@ -22,18 +23,14 @@ curr_quat = currpose[3:]
 r = R.from_quat(curr_quat) # convert quat to euler
 curr_euler = r.as_euler('xyz', degrees=False)
 
-# Break above pick pose into position and euler angles
-above_pick_position = ABOVE_PICK_POSE[:3]
-above_pick_quat = ABOVE_PICK_POSE[3:]
-r = R.from_quat(above_pick_quat) # convert quat to euler
-above_pick_euler = r.as_euler('xyz', degrees=False)
-# TODO: Add motion to ABOVE_PICK_POSE
-
 # Break pick pose into position and euler angles
 pick_position = PICK_POSE[:3]
 pick_quat = PICK_POSE[3:]
 r = R.from_quat(pick_quat) # convert quat to euler
 pick_euler = r.as_euler('xyz', degrees=False)
+
+abovepickpose = PICK_POSE.copy()
+abovepickpose[2] += 0.1
 
 # Break above target pose into position and euler angles
 above_target_position = ABOVE_TARGET_POSE[:3]
@@ -46,7 +43,11 @@ diffeuler = lambda a, b: (a - b + np.pi) % (2 * np.pi) - np.pi  # radians
 diffpos = lambda a, b: np.array(a) - np.array(b)  # meters
 is_pose_close = lambda eu1, eu2, pos1, pos2: np.allclose(diffeuler(eu1, eu2), [0, 0, 0], atol=0.1) and np.allclose(pos1, pos2, atol=0.01, rtol=0.01)
 
-# Send move command and wait until successful
+# Move above pick pose
+response = requests.post(SERVER_URL + "pose", json={"arr": ABOVE_PICK_POSE}, timeout=5) # must be list
+time.sleep(2)  # HACK: wait for robot to move
+
+# move to pick pose and wait until successful
 response = requests.post(SERVER_URL + "pose", json={"arr": PICK_POSE}, timeout=5) # must be list
 count = 0
 while not is_pose_close(curr_euler, pick_euler, curr_position, pick_position):
@@ -69,30 +70,44 @@ print('Closing gripper and moving above pick pose')
 gripperresponse = requests.post(SERVER_URL + "close_gripper", timeout=5)
 time.sleep(2) # wait for gripper to close (slower than gripper open)
 
-abovepickpose = PICK_POSE.copy()
-abovepickpose[2] += 0.1
 response = requests.post(SERVER_URL + "pose", json={"arr": abovepickpose}, timeout=5) # must be list
 time.sleep(2) # wait for gripper to close (slower than gripper open)
 
-print(f'Moving to above target pose {ABOVE_TARGET_POSE}')
+# print(f'Moving to above target pose {ABOVE_TARGET_POSE}')
 
-# Send move command and wait until successful
-response = requests.post(SERVER_URL + "pose", json={"arr": ABOVE_TARGET_POSE}, timeout=5) # must be list
-count = 0
-while not is_pose_close(curr_euler, above_target_euler, curr_position, above_target_position):
-    currpose = requests.post(SERVER_URL + "getpos", timeout=5).json()['pose']
-    curr_position = currpose[:3]
-    curr_quat = currpose[3:]
-    r = R.from_quat(curr_quat) # convert quat to euler
-    curr_euler = r.as_euler('xyz', degrees=False)
+# # Send move command and wait until successful
+# response = requests.post(SERVER_URL + "pose", json={"arr": ABOVE_TARGET_POSE}, timeout=5) # must be list
+# count = 0
+# while not is_pose_close(curr_euler, above_target_euler, curr_position, above_target_position):
+#     currpose = requests.post(SERVER_URL + "getpos", timeout=5).json()['pose']
+#     curr_position = currpose[:3]
+#     curr_quat = currpose[3:]
+#     r = R.from_quat(curr_quat) # convert quat to euler
+#     curr_euler = r.as_euler('xyz', degrees=False)
 
-    # print(f'Moving to above target pose {ABOVE_TARGET_POSE}')
-    print(f'Diff euler condition {diffeuler(curr_euler, above_target_euler)}')
-    print(f'Diff position condition {diffpos(curr_position, above_target_position)}')
-    time.sleep(1)
-    count += 1
-    if count > 30:
-        print("joint reset TIMEOUT")
-        break
+#     # print(f'Moving to above target pose {ABOVE_TARGET_POSE}')
+#     print(f'Diff euler condition {diffeuler(curr_euler, above_target_euler)}')
+#     print(f'Diff position condition {diffpos(curr_position, above_target_position)}')
+#     time.sleep(1)
+#     count += 1
+#     if count > 30:
+#         print("joint reset TIMEOUT")
+#         break
+
+print('waiting before final move')
+time.sleep(2)
+
+response = input('Kill old controller first. Then start roscore and press enter here')
+if response == 'q':
+    print('Exiting...')
+    exit()
+
+print('Setting joint goal...')
+setJointGoal = "rosparam set /target_joint_positions '[0.19097669621114738, 0.6011634117511281, -0.30170746777767066, -1.8931544224643337, 0.26372237982352575, 2.5055904395050472, 0.5184927675111416]' #ABOVE_TARGET_Q"
+os.system(setJointGoal)
+
+print('Starting joint controller...')
+sendGoal = "roslaunch serl_franka_controllers joint.launch robot_ip:=192.170.10.4 load_gripper:=true"
+os.system(sendGoal)
 
 print('Done!')
